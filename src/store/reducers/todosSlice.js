408c23e22cf40546fe58ccdbfb8ce5b494cae3c6
GiangPhanA <daperidot@gmail.com>
2022-01-21 15:01:07 +0700

    add full

--- src/store/reducers/todosSlice.js
+++ src/store/reducers/todosSlice.js
@@ -1,5 +1,32 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, nanoid } from '@reduxjs/toolkit'
 import axios from 'axios'
+
+// Reducer thunk
+export const getTodos = createAsyncThunk('todos/todosFetched', async () => {
+	const response = await axios.get('https://61b0bbb13c954f001722a5fb.mockapi.io/api/todo'
+	)
+	return response.data
+})
+
+export const addTodo = createAsyncThunk('todos/todoAdded', async title => {
+	const newTodo = {
+		id: nanoid(),
+		title,
+		completed: false
+	}
+
+	await axios.post('https://61b0bbb13c954f001722a5fb.mockapi.io/api/todo', newTodo)
+
+	return newTodo
+})
+
+
+export const deleteTodo = createAsyncThunk('todos/todoDeleted', async todoId => {
+	await axios.delete(`https://61b0bbb13c954f001722a5fb.mockapi.io/api/todo/${todoId}`)
+
+	return todoId
+})
+
 
 const todosSlice = createSlice({
     name: 'todos',
@@ -9,20 +36,20 @@ const todosSlice = createSlice({
     // khai bao 1 cv moi reducer bao gom action creator, acton, reducer
     reducers: {
         // reducer khong duoc chua thanh phan ngau nhien
-        addTodo: { 
-            reducer(state, action) {
-              state.allTodos.unshift (action.payload)            
-            },
-            prepare(title){
-                return {
-                    payload: {
-                        id: nanoid(),
-                        title,
-                        completed: false
-                    }
-                }
-            }   
-        },
+        // addTodo: { 
+        //     reducer(state, action) {
+        //       state.allTodos.unshift (action.payload)            
+        //     },
+        //     prepare(title){
+        //         return {
+        //             payload: {
+        //                 id: nanoid(),
+        //                 title,
+        //                 completed: false
+        //             }
+        //         }
+        //     }   
+        // },
         markComplete(state, action) {
             
             const todoId = action.payload
@@ -32,10 +59,10 @@ const todosSlice = createSlice({
             })
             
         },
-        deleteTodo(state, action) {
-			const todoId = action.payload
-			state.allTodos = state.allTodos.filter(todo => todo.id !== todoId)
-		},
+        // deleteTodo(state, action) {
+		// 	const todoId = action.payload
+		// 	state.allTodos = state.allTodos.filter(todo => todo.id !== todoId)
+		// },
         // async getAllTodos(state, action) {
         //     try {
         //         const response = await axios.get('https://61b0bbb13c954f001722a5fb.mockapi.io/api/todo')
@@ -46,26 +73,63 @@ const todosSlice = createSlice({
         //     }
         // }
 
-        todosFetched(state, action) {
+        /* todosFetched(state, action) {
             state.allTodos = action.payload
-        }
+        } */
+    },
+    extraReducers: {
+        [getTodos.pending]:(state, action) => {
+            console.log('fetch todo form api')
+        },
+        [getTodos.fulfilled]:(state, action) => {
+            console.log('done')
+            state.allTodos = action.payload
+        },
+        [getTodos.rejected]: (state, action) => {
+			console.log('Failed to get todos!!!')
+		},
+        [addTodo.fulfilled]: (state, action) => {
+			state.allTodos.unshift(action.payload)
+		},
+        [addTodo.rejected]: (state, action) => {
+			console.log('Failed to post todos!!!')
+		},
+
+        [deleteTodo.fulfilled]: (state, action) => {
+			const todoId = action.payload
+			state.allTodos = state.allTodos.filter(todo => todo.id !== todoId)
+		},
+        [deleteTodo.rejected]: (state, action) => {
+			console.log('Failed to delete todos!!!')
+		}
     }
 })
 
 // Asynnc action creator, action and reducer dispatch
-export const getTodos = () => {
-    const getTodosAsync = async dispatch => {
-        try {
-            const response = await axios.get('https://61b0bbb13c954f001722a5fb.mockapi.io/api/todo')
-            dispatch(todosFetched(response.data))
-        }
-        catch (error) {
-            console.log(error)
-        }
+// export const getTodos = () => {
+//     const getTodosAsync = async dispatch => {
+//         try {
+//             const response = await axios.get('https://61b0bbb13c954f001722a5fb.mockapi.io/api/todo')
+//             dispatch(todosFetched(response.data))
+//         }
+//         catch (error) {
+//             console.log(error)
+//         }
 
-    }
-    return getTodosAsync
-}
+//     }
+//     return getTodosAsync
+// }
+
+// export const getTodos = () => async dispatch => {
+//     try {
+//         const response = await axios.get('https://61b0bbb13c954f001722a5fb.mockapi.io/api/todo')
+//         dispatch(todosFetched(response.data))
+//     }
+//     catch (error) {
+//         console.log(error)
+//     }
+
+// }
 
 // Reducer
 const todosReducer = todosSlice.reducer
@@ -74,7 +138,12 @@ const todosReducer = todosSlice.reducer
 export const todosSelector = state => state.todosReducer.allTodos
 
 //export action
-export const {addTodo, markComplete, deleteTodo, todosFetched} = todosSlice.actions
+export const {
+    // addTodo, 
+    markComplete, 
+    // deleteTodo, 
+    // todosFetched
+} = todosSlice.actions
 
 //Export reducer
 
